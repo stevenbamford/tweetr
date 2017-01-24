d01ae892fde9417ccb4820afcbad019c7b22792d
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-24 17:04:39 +0000

    Modified css on hover state of individual tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,19 +9,12 @@ $(document).ready(function (){
   $(".tweet").on("mouseover", function(){
 
     $(".tweet footer div").css("display", "inline");
-    $("article.tweet h2").css("color", "#244751");
-    $("article.tweet").css("border", "1px solid #244751");
-    $("article.tweet h3" ).css("color", "#244751");
-    $("article.tweet img").css("opacity", 1);
-
+    $("article.tweet header").css("opacity", "1");
   }).on("mouseleave", function(){
 
-    $(".tweet footer div").css("display", "none");
-    $("article.tweet h2").css("color", "#667E85");
-    $("article.tweet").css("border", "1px solid #ADADAD");
-    $("article.tweet h3" ).css("color", "#ADADAD");
-    $("article.tweet img").css("opacity", 0.5);
+     $(".tweet footer div").css("display", "none");
+     $("article.tweet header").css("opacity", "0.5");
 
-  })
+  });
 
 });
