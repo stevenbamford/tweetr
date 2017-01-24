ea545c05bba51236579c32c19094e70ce4934832
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-24 05:54:55 +0000

    Created template for what individual tweets will look like on index. Tweet changes appearance on hover.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,24 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function (){
+
+  $(".tweet").on("mouseover", function(){
+
+    $(".tweet footer div").css("display", "inline");
+    $("article.tweet h2").css("color", "#244751");
+    $("article.tweet").css("border", "1px solid #244751");
+    $("article.tweet h3" ).css("color", "#244751");
+    $("article.tweet img").css("opacity", 1);
+
+  }).on("mouseleave", function(){
+
+    $(".tweet footer div").css("display", "none");
+    $("article.tweet h2").css("color", "#667E85");
+    $("article.tweet").css("border", "1px solid #ADADAD");
+    $("article.tweet h3" ).css("color", "#ADADAD");
+    $("article.tweet img").css("opacity", 0.5);
+
+  })
+
+});
