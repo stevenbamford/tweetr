a43b8a57f6bc3151864cd81519660daecd4a78fd
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-25 18:57:27 +0000

    Refactored app.js and css files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,6 @@
 $(document).ready(function (){
 
   $("#nav-bar button").on("click", function (){
-    console.log("button clicked");
     $(".new-tweet").slideToggle("fast");
     $(".new-tweet textarea").focus();
   });
@@ -16,29 +15,30 @@ $(document).ready(function (){
 
   $(".new-tweet form").on("submit", function(event){
 
-    if($(this).closest("textarea").context["0"].textLength > 140){
+    if($(this).find("textarea").val().length > 140){
        event.preventDefault();
        alert("Too many characters");
        return;
      }
 
-    if(!$(this).closest("textarea").context["0"].value){
+    if(!$(this).find("textarea").val()){
       event.preventDefault();
       alert("Please enter a tweet!");
       return;
     }
-   let data = $(this).serialize();
-   event.preventDefault();
+
+    event.preventDefault();
     $.ajax({
       url: "/tweets",
       method: "POST",
-      data: data,
-      success: function(){
+      data: $(this).serialize(),
+      }).then(function(){
         $("#tweets-container").empty();
         loadTweets();
-      }
+      });
+
+      $(".new-tweet textarea").val("");
     });
-  });
 
   $("article.tweet").on("mouseenter", function(){
     $(this).find("footer div").css("display", "inline");
@@ -54,54 +54,52 @@ $(document).ready(function (){
     $.ajax({
       url: "/tweets",
       method: 'GET',
-      success: function (response) {
-        renderTweets(response);
-      }
-    });
+      }).then(function(response){
+         renderTweets(response);
+      });
   };
   loadTweets();
-});
-
-function calcWhenTweeted(tweetObj){
-  let dateCreated = Date.now() - tweetObj["created_at"];
-  let createdAgo = dateCreated / 1000 ;
-
-  if(createdAgo > 31557600){
-    createdAgo = Math.floor(createdAgo/ 3155760);
-    return createdAgo + " years ago";
-  }
-  if(createdAgo > 86400){
-    createdAgo = Math.floor(createdAgo/ 86400);
-    return createdAgo + " days ago";
-  }
-  if(createdAgo > 3600){
-    createdAgo = Math.floor(createdAgo/ 3600);
-    return createdAgo + " hours ago";
-  }
-  if(createdAgo > 60){
-    createdAgo = Math.floor(createdAgo/ 60);
-    return createdAgo + " minutes ago";
-  }
-  if(createdAgo < 60){
-    createdAgo = Math.floor(createdAgo);
-    return createdAgo + " seconds ago";
-  }
-}
 
 
-function appendToHeader(tweetObj){
+ function calcWhenTweeted(tweetObj){
+   let dateCreated = Date.now() - tweetObj["created_at"];
+   let createdAgo = dateCreated / 1000 ;
+
+   if(createdAgo > 31557600){
+     createdAgo = Math.floor(createdAgo/ 3155760);
+     return createdAgo + " years ago";
+   }
+   if(createdAgo > 86400){
+     createdAgo = Math.floor(createdAgo/ 86400);
+     return createdAgo + " days ago";
+   }
+   if(createdAgo > 3600){
+     createdAgo = Math.floor(createdAgo/ 3600);
+     return createdAgo + " hours ago";
+   }
+   if(createdAgo > 60){
+     createdAgo = Math.floor(createdAgo/ 60);
+     return createdAgo + " minutes ago";
+   }
+   if(createdAgo < 60){
+     createdAgo = Math.floor(createdAgo);
+     return createdAgo + " seconds ago";
+   }
+ }
+
+
+ function appendToHeader(tweetObj){
   const $tweetHeader = $("<header>");
   const imgSrc = tweetObj.user.avatars.small;
-  const $avatar = $("<img>").attr("src", imgSrc);
-  const $userName = $("<h2>").text(tweetObj.user.name);
-  const $handle = $("<h3>").text(tweetObj.user.handle);
 
-  $tweetHeader.append($avatar, $userName, $handle);
+  $("<img>").attr("src", imgSrc).appendTo($tweetHeader);
+  $("<h2>").text(tweetObj.user.name).appendTo($tweetHeader);
+  $("<h3>").text(tweetObj.user.handle).appendTo($tweetHeader);
 
   return $tweetHeader;
-}
+ }
 
-function appendToFooter(tweetObj){
+ function appendToFooter(tweetObj){
   const timeWhenTweeted = calcWhenTweeted(tweetObj);
   const $tweetFooter = $("<footer>" + timeWhenTweeted + "</footer>");
   const $tweetFooterIcons = $("<div>");
@@ -113,34 +111,34 @@ function appendToFooter(tweetObj){
   $tweetFooter.append($tweetFooterIcons);
 
   return $tweetFooter;
-}
-
- function createTweetElement(tweetObj){
-  const $tweet = $("<article>").addClass("tweet");
-  const header = appendToHeader(tweetObj);
-  const tweetContent = $("<p>").text(tweetObj.content.text);
-  const footer = appendToFooter(tweetObj);
-
-  $tweet.append(header, tweetContent, footer);
-
-  $($tweet).on("mouseenter", function(){
-
-    $(this).find("footer div").css("display", "inline");
-    $(this).find("header").css("opacity", "1");
-
-  }).on("mouseleave", function(){
-     $(this).find("footer div").css("display", "none");
-     $(this).find("header").css("opacity", "0.5");
-  });
-
-  return $tweet;
- };
-
- function renderTweets(arrOfObjects){
-  arrOfObjects.forEach(function(object){
-    $("#tweets-container").prepend(createTweetElement(object));
-  });
  }
 
+  function createTweetElement(tweetObj){
+   const $tweet = $("<article>").addClass("tweet");
+   const header = appendToHeader(tweetObj);
+   const tweetContent = $("<p>").text(tweetObj.content.text);
+   const footer = appendToFooter(tweetObj);
 
+   $tweet.append(header, tweetContent, footer);
+
+   $($tweet).on("mouseenter", function(){
+
+     $(this).find("footer div").css("display", "inline");
+     $(this).find("header").css("opacity", "1");
+
+   }).on("mouseleave", function(){
+      $(this).find("footer div").css("display", "none");
+      $(this).find("header").css("opacity", "0.5");
+   });
+
+   return $tweet;
+  };
+
+  function renderTweets(arrOfObjects){
+   arrOfObjects.forEach(function(object){
+     $("#tweets-container").prepend(createTweetElement(object));
+   });
+  }
+
+});
 
