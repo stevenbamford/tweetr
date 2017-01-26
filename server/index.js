25e9a6e816ddd4db1941b84f179c5c5ea8dbc5c2
Steven Bamford <sbamford@alumni.ubc.ca>
2017-01-26 18:41:08 +0000

    Revising code and removing unecessary comments

--- server/index.js
+++ server/index.js
@@ -25,8 +25,6 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
   const dataHelpers = makeDataHelpers(db);
 
-  // const db = require("./lib/in-memory-db");
-
   // The `data-helpers` module provides an interface to the database of tweets.
   // This simple interface layer has a big benefit: we could switch out the
   // actual database it uses and see little to no changes elsewhere in the code
